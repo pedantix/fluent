e47c915a8c61156df4a1b2e50f089acc0f0fad64
Shaun Hubbard <shaun@splife.com>
2018-03-28 14:23:35 -0400

    added ilike, pending a PR on Database Kit

--- Sources/FluentSQL/QueryComparison.swift
+++ Sources/FluentSQL/QueryComparison.swift
@@ -89,6 +89,36 @@ public func ~~ <Model, Value>(lhs: KeyPath<Model, Value>, rhs: String) throws ->
     return try _contains(lhs, .like, .data("%\(rhs)%"))
 }
 
+// BLOCKING TODO: Switchout the following to .ilike on approval, this is just to show intent
+let ilike = DataPredicateComparison.sql("ilike")
+
+// Case Insensitive containment
+infix operator ^~=
+/// Has prefix
+public func ^~= <Model, Value>(lhs: KeyPath<Model, Value>, rhs: String) throws -> ModelFilter<Model>
+    where Model.Database.QueryFilter: DataPredicateComparisonConvertible
+{
+    return try _contains(lhs, ilike, .data("%\(rhs)"))
+}
+
+infix operator ^=~
+/// Has suffix.
+public func ^=~ <Model, Value>(lhs: KeyPath<Model, Value>, rhs: String) throws -> ModelFilter<Model>
+    where Model.Database.QueryFilter: DataPredicateComparisonConvertible
+{
+    return try _contains(lhs, ilike, .data("\(rhs)%"))
+}
+
+infix operator ^~~
+/// Contains.
+public func ^~~ <Model, Value>(lhs: KeyPath<Model, Value>, rhs: String) throws -> ModelFilter<Model>
+    where Model.Database.QueryFilter: DataPredicateComparisonConvertible
+{
+    return try _contains(lhs, ilike, .data("%\(rhs)%"))
+}
+
+
+
 /// Operator helper func.
 private func _contains<M, V>(_ key: KeyPath<M, V>, _ comp: DataPredicateComparison, _ value: QueryFilterValue<M.Database>) throws -> ModelFilter<M>
     where M.Database.QueryFilter: DataPredicateComparisonConvertible
